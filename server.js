6cf4e520667921457c91e070c567c3394d5432b0
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-11-20 10:32:05 -0300

    conectando com o banco de dados

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-import express from 'express';
+import express from "express"; // Importa o módulo Express para criar o servidor web.
 
-const posts = [
-    { id: 1, descricao: "Gato 1", imagem: "https://placecats.com/millie/300/150"},
-    { id: 2, descricao: "Gato 2", imagem: "https://placecats.com/millie/300/150"},
-    { id: 3, descricao: "Gato 3", imagem: "https://placecats.com/millie/300/150"}
-  ];
+import conectarAoBanco from "./src/config/dbConfig.js"; // Importa a função para conectar ao banco de dados.
 
-const app = express(); 
-app.use(express.json());
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO); // Conecta ao banco de dados usando a string de conexão do ambiente.
 
-app.listen(3000, () => {
-    console.log("Servidor escutando!");
+const app = express(); // Cria uma instância do servidor Express.
+app.use(express.json()); // Habilita o middleware para analisar o corpo das requisições JSON.
+
+app.listen(3000, () => { // Inicia o servidor na porta 3000.
+    console.log("Servidor escutando!"); // Imprime uma mensagem no console indicando que o servidor está ativo.
 });
 
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
-
-function buscarPost(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
+async function getPosts() { // Função assíncrona para buscar os posts do banco de dados.
+    const db = conexao.db("imersaoAlura"); // Seleciona o banco de dados "imersaoAlura".
+    const colecao = db.collection("posts"); // Seleciona a coleção "posts" dentro do banco de dados.
+    return colecao.find().toArray(); // Busca todos os documentos da coleção e retorna um array.
 }
 
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPost(req.params.id)
-    res.status(200).json(posts[index]);
+app.get("/posts", async (req, res) => { // Define uma rota GET para "/posts".
+    const posts = await getPosts(); // Chama a função para buscar os posts.
+    res.status(200).json(posts); // Envia os posts como resposta JSON com o status 200 (OK).
 });
