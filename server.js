ab4bb484d7854ad199f174acb84f67f56a149622
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-11-19 23:18:01 -0300

    criando api

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 import express from 'express';
 
-const app = express();
+const posts = [
+    { id: 1, descricao: "Gato 1", imagem: "https://placecats.com/millie/300/150"},
+    { id: 2, descricao: "Gato 2", imagem: "https://placecats.com/millie/300/150"},
+    { id: 3, descricao: "Gato 3", imagem: "https://placecats.com/millie/300/150"}
+  ];
+
+const app = express(); 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando!");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Imersão Alura Bruna");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function buscarPost(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+}
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPost(req.params.id)
+    res.status(200).json(posts[index]);
 });
