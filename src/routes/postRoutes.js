8b31b78b1bc4fc514d29942967733c5f53a92b71
Bruna Borges <bruna.lborges.matias@gmail.com>
2024-11-22 20:50:41 -0300

    instalando cors

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import { listarPosts, postarPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
+
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
@@ -15,7 +22,8 @@ const upload = multer({dest:"./uploads", storage})
 
 const routes = (app) => {
     app.use(express.json()); // Habilita o middleware para analisar o corpo das requisições JSON.
-    
+    app.use(cors(corsOptions));
+
     app.get("/posts", listarPosts);
     
     app.post("/posts", postarPost);
